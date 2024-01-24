1bf304d7e3c02e01135a2f9a772955d521d0b2c1
Anghel3m <adms95@outlook.es>
2024-01-23 22:29:20 -0500

    carga del protecto lista de tareas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TareasService } from './sevices/tareas.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'lista-tareas';
+export class AppComponent implements OnInit {
+  listaTareas: String[]=[];
+  nuevaTarea: string = '';
+ 
+
+  private _tareasService =inject(TareasService);
+
+
+  ngOnInit(): void {
+    this.listaTareas = this._tareasService.getTareas();
+    
+  };
+
+  agregarTarea(){
+    this._tareasService.agregarTarea(this.nuevaTarea);
+    this.nuevaTarea = '';
+    this.listaTareas = this._tareasService.getTareas(); 
+  };
+
+  eliminarTarea(index: number){
+    this._tareasService.eliminarTarea(index);
+    this.listaTareas = this._tareasService.getTareas(); 
+
+
+  };
+
 }
